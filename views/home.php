5ae3b3d767b3a4b5b57f95395e8b71db8aea6d33
DamianoMura <mura.damiano14@gmail.com>
2025-07-09 14:04:54 +0200

    [upd] home content

--- views/home.php
+++ views/home.php
@@ -2,13 +2,27 @@
   <div class="row">
     <div class="col-12">
       <div class="text-center py-2">
-        <h1>JDWdev.it</h1>
-        <h3>
-          <\per codex ad astram>
-        </h3>
-        <p>
+        <div class="container">
+          <div class="row">
+            <div class="col-12">
+              <h1>JDWdev.it</h1>
+              <h3>
+                <\per codex ad astram>
+              </h3>
+            </div>
+            <div class="col-6">
+              <img src="../assets/img/logo-jdw-trans-black.png" width="100%" alt="" id="main-logo">
 
-        </p>
+            </div>
+            <div class="col-6 d-flex flex-column justify-content-center align-items-center py-3">
+              <h2>John Demian Walls Dev</h2>
+              <span class="text-primary">created by Damiano Mura</span>
+              <p>
+
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
